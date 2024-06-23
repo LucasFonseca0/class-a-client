a3ef9520b9a5574f1c4616880476e2b591e90c6c
Lucas Fonseca <lucas.ribeiro.24444@gmail.com>
2024-06-23 13:29:52 +0100

    Fix: Newsletter changed to ContactForm

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -7,7 +7,7 @@ import Hero from '@/components/hero'
 import Features from '@/components/features'
 import FeaturesBlocks from '@/components/features-blocks'
 import Testimonials from '@/components/testimonials'
-import Newsletter from '@/components/newsletter'
+import ContactForm from '@/components/ContactForm'
 
 export default function Home() {
   return (
@@ -16,7 +16,7 @@ export default function Home() {
       <Features />
       <FeaturesBlocks />
       <Testimonials />
-      <Newsletter />
+      <ContactForm />
     </>
   )
 }
