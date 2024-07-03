fa3d7e146ae44aab88d21699853b19ac5c2b9e47
Lucas Fonseca <lucas.ribeiro.24444@gmail.com>
2024-07-03 06:25:38 +0100

    fix: logo background changed

--- components/hero.tsx
+++ components/hero.tsx
@@ -46,7 +46,7 @@ export default function Hero() {
       </div>
 
       {/* Curved Background Section */}
-      <div className="relative w-full mb-12 md:mb-20 bg-gray-600">
+      <div className="relative w-full mb-12 md:mb-20 bg-gray-500">
         <div className="wave-top rotate-180">
           <svg viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg" className="w-full h-32">
             <path fill="#ffffff" fillOpacity="1" d="M0,128L48,154.7C96,181,192,235,288,234.7C384,235,480,181,576,181.3C672,181,768,235,864,218.7C960,203,1056,117,1152,101.3C1248,85,1344,139,1392,165.3L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
