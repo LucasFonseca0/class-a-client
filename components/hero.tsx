9922d05813169a9912ce422e00d1e004ac83b5b2
Lucas Fonseca <lucas.ribeiro.24444@gmail.com>
2024-07-03 06:10:59 +0100

    fix: removed css styles

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,9 +1,8 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function Hero() {
   return (
-    <section className="relative" id='hero'>
-
+    <section className="relative" id="hero">
       {/* Illustration behind hero content */}
       <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none -z-1" aria-hidden="true">
         <svg width="1360" height="578" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
@@ -22,15 +21,17 @@ export default function Hero() {
       </div>
 
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
-
         {/* Hero content */}
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-
           {/* Section header */}
           <div className="text-center pb-12 md:pb-16">
-            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Transforme sua <span className="bg-clip-text text-transparent bg-gradient-to-l from-orange-500 to-red-400">presença digital</span></h1>
+            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">
+              Transforme sua <span className="bg-clip-text text-transparent bg-gradient-to-l from-orange-500 to-red-400">presença digital</span>
+            </h1>
             <div className="max-w-3xl mx-auto">
-              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Nossas soluções de marketing digital são eficazes e sustentáveis, garantindo sucesso duradouro com mínimo esforço.</p>
+              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">
+                Nossas soluções de marketing digital são eficazes e sustentáveis, garantindo sucesso duradouro com mínimo esforço.
+              </p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
                   <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="#contact">Entre em contato</a>
@@ -41,12 +42,25 @@ export default function Hero() {
               </div>
             </div>
           </div>
-
-          <Image src={"/images/LogoNormal.png"} height={1000} width={1000} alt='Logo classA' className='bg-gray-700 m-auto w-80 rounded-md' ></Image>
-
         </div>
+      </div>
 
+      {/* Curved Background Section */}
+      <div className="relative w-full mb-12 md:mb-20 bg-gray-600">
+        <div className="wave-top rotate-180">
+          <svg viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg" className="w-full h-32">
+            <path fill="#ffffff" fillOpacity="1" d="M0,128L48,154.7C96,181,192,235,288,234.7C384,235,480,181,576,181.3C672,181,768,235,864,218.7C960,203,1056,117,1152,101.3C1248,85,1344,139,1392,165.3L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+          </svg>
+        </div>
+        <div className=" h-32 ">
+          <Image src="/images/LogoNormal.png" height={1000} width={1000} alt="Logo classA" className="absolute top-[45%] left-1/2 -translate-x-1/2 -translate-y-1/2 w-1/2  lg:w-1/3 rounded-md" />
+        </div>
+        <div className="wave-bottom rotate-180">
+          <svg viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg" className="w-full h-32">
+            <path fill="#ffffff" fillOpacity="1" d="M0,224L48,208C96,192,192,160,288,170.7C384,181,480,235,576,256C672,277,768,267,864,234.7C960,203,1056,149,1152,138.7C1248,128,1344,160,1392,186.7L1440,213.3L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
+          </svg>
+        </div>
       </div>
     </section>
-  )
+  );
 }
