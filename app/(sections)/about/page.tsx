1dbbb1f213ec514c231cc49566dde96b6fb19b5f
Lucas Fonseca <lucas.ribeiro.24444@gmail.com>
2024-07-04 05:56:28 +0100

    feat: created about us text and styles

--- app/(sections)/about/page.tsx
+++ app/(sections)/about/page.tsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <main className='text-black'>
-      aqui eu falei sobre a empresa
-    </main>
-  )
-}
+    <main className="text-gray-900  py-14 px-5 sm:px-10">
+      <div className="max-w-4xl mx-auto bg-white p-8 rounded-lg ">
+        <h2 className="text-3xl font-bold mb-4">Sobre Nós</h2>
+        <p className="mb-6">
+          Bem-vindo à nossa agência de marketing digital! Somos apaixonados por
+          ajudar empresas a alcançar seu máximo potencial online. Nossa missão é
+          fornecer soluções eficazes e criativas que impulsionem o crescimento e
+          o sucesso dos nossos clientes.
+        </p>
+        <h3 className="text-2xl font-semibold mb-4">Nossa História</h3>
+        <p className="mb-6">
+          Nossa agência nasceu da vontade de transformar a maneira como as
+          empresas se conectam com seus clientes. Somos uma equipe de
+          especialistas apaixonados por marketing digital, cada um trazendo uma
+          rica experiência e conhecimento. Queremos usar essa expertise para
+          ajudar você a alcançar resultados incríveis. Para nós, cada projeto é
+          uma chance de inovar, aplicar nossas habilidades e superar suas
+          expectativas.
+        </p>
 
-export default page
+        <h3 className="text-2xl font-semibold mb-4">O Que Nos Move</h3>
+        <p className="mb-6">
+          Nosso compromisso é com a excelência e a inovação. Acreditamos que uma
+          estratégia bem elaborada pode fazer toda a diferença no mundo digital.
+          Por isso, estamos sempre atualizados com as últimas tendências e
+          tecnologias para oferecer o melhor serviço aos nossos clientes.
+        </p>
+        <h3 className="text-2xl font-semibold mb-4">Nossos Valores</h3>
+        <ul className="list-disc list-inside mb-6 pl-4">
+          <li className="mb-2">
+            <strong>Inovação:</strong> Buscamos constantemente novas ideias e
+            abordagens para melhorar nossas estratégias.
+          </li>
+          <li className="mb-2">
+            <strong>Resultados:</strong> Nosso foco está em entregar resultados
+            mensuráveis e significativos para nossos clientes.
+          </li>
+          <li className="mb-2">
+            <strong>Transparência:</strong> Valorizamos a comunicação clara e
+            aberta com nossos clientes em todas as etapas do projeto.
+          </li>
+          <li className="mb-2">
+            <strong>Paixão:</strong> Amamos o que fazemos, por isso acreditamos
+            muito na nossa empresa.
+          </li>
+        </ul>
+        <h3 className="text-2xl font-semibold mb-4">Nossa Equipe</h3>
+        <p className="mb-6">
+          Nossa equipe é composta por especialistas talentosos em diversas
+          áreas, incluindo gestão de tráfego, criação de sites e design gráfico.
+          Juntos, trabalhamos para oferecer soluções integradas e personalizadas
+          que atendam às necessidades específicas de cada cliente.
+        </p>
+        <h3 className="text-2xl font-semibold mb-4">Entre em Contato</h3>
+        <p>
+          Se você está pronto para levar sua empresa ao próximo nível, entre em
+          contato conosco. Estamos ansiosos para colaborar e alcançar resultados
+          extraordinários juntos!
+        </p>
+      </div>
+    </main>
+  );
+};
+
+export default page;
