89d2f0dd719f9a2d402f3c358bf54d08c153658d
Lucas Fonseca <lucas.ribeiro.24444@gmail.com>
2024-07-23 21:22:53 +0100

    feat: added some logs to debug on vercel

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -4,20 +4,31 @@ import { verifyPassword, generateToken } from '../../../lib/auth';
 const USERNAME = process.env.ADMIN_USERNAME as string;
 const PASSWORD_HASH = process.env.ADMIN_PASSWORD_HASH as string;
 
+console.log('USERNAME:', USERNAME);
+console.log('PASSWORD_HASH:', PASSWORD_HASH);
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
-  console.log(PASSWORD_HASH)   
-  const { username, password }: { username: string; password: string } = await request.json();
+  try {
+    const { username, password }: { username: string; password: string } = await request.json();
 
-  if (username !== USERNAME) {
-    return NextResponse.json({ success: false, error: 'Invalid credentials' }, { status: 401 });
+    console.log('Received username:', username);
+    console.log('Received password:', password);
+
+    if (username !== USERNAME) {
+      console.log('Invalid username');
+      return NextResponse.json({ success: false, error: 'Invalid credentials' }, { status: 401 });
+    }
+
+    const isValid = await verifyPassword(password, PASSWORD_HASH);
+    if (!isValid) {
+      console.log('Invalid password');
+      return NextResponse.json({ success: false, error: 'Invalid credentials' }, { status: 401 });
+    }
+
+    const token = generateToken({ username: USERNAME });
+    return NextResponse.json({ success: true, token });
+  } catch (error) {
+    console.error('Error processing request:', error);
+    return NextResponse.json({ success: false, error: 'Internal Server Error' }, { status: 500 });
   }
-
-  const isValid = await verifyPassword(password, PASSWORD_HASH);
-  if (!isValid) {
-    return NextResponse.json({ success: false, error: 'Invalid credentials' }, { status: 401 });
-  }
-
-  const token = generateToken({ username: USERNAME });
-  return NextResponse.json({ success: true, token });
 }
